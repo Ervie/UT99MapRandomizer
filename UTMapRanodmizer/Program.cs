36f985df2b77cabb5db6ed431e65e23363655c52
Ervie <bbuchala93@gmail.com>
2019-10-20 15:51:14 +0200

    Added reading values from config.

--- UTMapRanodmizer/Program.cs
+++ UTMapRanodmizer/Program.cs
@@ -1,15 +1,24 @@
 ﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace UTMapRanodmizer
 {
 	internal class Program
 	{
+		private static Options cmdOptions;
+		private static IConfiguration config;
+
 		private static void Main(string[] args)
 		{
+			config = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.Build();
+
 			Parser.Default.ParseArguments<Options>(args)
 				.WithParsed(o =>
 				{
+					cmdOptions = o;
 					if (o.Repeat)
 					{
 						Console.WriteLine($"Read value: {o.Repeat}");
